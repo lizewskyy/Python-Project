f001a838c6e825452b71358b6da251f7329e6d8b
lizewskyy <gm224113@gmail.com>
2025-03-19 05:22:15 -0400

    Create: Sign up working, need to make login

--- website/auth.py
+++ website/auth.py
@@ -1,13 +1,47 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db   ##means from __init__.py import db
+from flask_login import login_user, login_required, logout_user, current_user
+
+
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
     return render_template("login.html")
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be more than 4 characters.', category="error")
+            pass
+        elif len(username) < 2:
+            flash('Username must be more than 2 Characters', category="error")
+            pass
+        elif password1 != password2:
+            flash('Password is not the Same', category="error")
+            pass
+        elif len(password1) < 7:
+            flash('Password needs to be more than 7 Characters', category="error")
+            pass
+        else:
+            new_user = User(email=email, username=username, password=generate_password_hash(password1, method='scrypt'))
+            db.session.add(new_user)
+            db.session.commit() 
+
+            flash('Account Created!', category="success")
+
+            return redirect(url_for('views.home'))
+
+            #add to db
     return render_template("sign-up.html")
