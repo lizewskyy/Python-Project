7528de6ece955d758a5811bef681d3a2347dbab7
lizewskyy <gm224113@gmail.com>
2025-03-20 00:46:46 -0400

    fixed code

--- website/auth.py
+++ website/auth.py
@@ -5,7 +5,6 @@ from . import db   ##means from __init__.py import db
 from flask_login import login_user, login_required, logout_user, current_user
 
 
-
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET','POST'])
